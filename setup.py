989712d7cff272c5389372b8f0243ab4b2a0932f
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-06-02 19:17:31 +0300

    feat(pima-dataset) data science
    - building a predictive model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using machine learning workflow to process and transform Pima India Data to create predictive model. This model must prediict which people are likely to develop diabetes with 70% or more accuracy',
+    author='Hesbon',
+    license='MIT',
+)
